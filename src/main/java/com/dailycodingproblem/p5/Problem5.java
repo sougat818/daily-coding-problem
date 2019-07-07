cd756bfa1fe3f0b93f3da6d5f5eba513e5da9916
Sougata Khan <sougat818@gmail.com>
2019-07-07 13:07:35 +1000

    Problem 5

---
+++ src/main/java/com/dailycodingproblem/p5/Problem5.java
@@ -0,0 +1,26 @@
+package com.dailycodingproblem.p5;
+
+/**
+ * cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of
+ * that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4. Given this
+ * implementation of cons:
+ * <pre>
+ * def cons(a, b):
+ *     def pair(f):
+ *         return f(a, b)
+ *     return pair
+ * </pre>
+ * Implement car and cdr.
+ */
+
+class Problem5 {
+
+  /*
+  Time Complexity - O(?)
+  Space Complexity - O(?)
+  Auxiliary Space - O(?)
+   */
+  void solution() {
+  }
+
+}
---
+++ src/test/java/com/dailycodingproblem/p5/Problem5Test.java
@@ -0,0 +1,19 @@
+package com.dailycodingproblem.p5;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Problem5Test {
+
+  Problem5 problem5;
+
+  @Before
+  public void setUp() {
+    problem5 = new Problem5();
+  }
+
+  @Test
+  public void testSolution() {
+    problem5.solution();
+  }
+}
